144a04f485061dcd02d800214a61b58f5c633a8b
Vitalii Semenenko <SemenenkoSVAT@gmail.com>
2021-11-29 00:10:11 +0200

    created react app and draft of first task

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import 'normalize.css';
+import './App.css';
+import SocialPofile from './components/SocialProfile/SocialProfile';
+
+function App() {
+  return <SocialPofile></SocialPofile>;
+}
+
+export default App;
