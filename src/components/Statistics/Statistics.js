94fe4ce6c57289121bb7dc7a24c237069a0dd46c
Vitalii Semenenko <SemenenkoSVAT@gmail.com>
2021-12-01 02:34:25 +0200

    second task

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,19 @@
+import onRandomColor from '../helpers/onRandomColor';
+
+function Statistics({ title, stats }) {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+      <ul className="stat-list">
+        {stats.map(stat => (
+          <li className="item" key={stat.id} bgColor={onRandomColor()}>
+            <span class="label">{stat.label}</span>
+            <span class="percentage">{stat.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Statistics;
