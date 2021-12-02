970b46e910d573e4ce85d4a5254b8ab8c8f996a0
Vitalii Semenenko <SemenenkoSVAT@gmail.com>
2021-12-02 16:58:07 +0200

    added the forth task

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Statistics from './components/Statistics/Statistics';
 import data from './components/Statistics/data.json';
 import FriendList from './components/FriendList/FriendList';
 import friends from './components/FriendList/friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/transactions.json';
 
 function App() {
   return (
@@ -23,6 +25,7 @@ function App() {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </Fragment>
   );
 }
